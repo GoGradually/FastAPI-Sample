f395209003e50a94411528753af0836adf039f48
GoGradually <chkun3109@gmail.com>
2026-01-04 22:47:13 +0900

    refactor: fastapi 패키지 이름 변경

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import fastapi.main
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("fastapi.main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("startfastapi.main:app", host="localhost", port=8000, reload=True)
 
